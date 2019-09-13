514a65dd88a5bf352cef04fc397e01157da30ccb
lucabamo <36515511+lucabamo@users.noreply.github.com>
2019-09-13 11:33:00 -0500

    Proyecto Administración de bases
    Primer commit

---
+++ sistemaTiendaAbarrotes/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace sistemaTiendaAbarrotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dgEmpleado = new System.Windows.Forms.DataGridView();
+            this.IdEmpleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreEmpleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaNacEmpleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edadEmpleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usuarioEmpleado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bEditar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.bEliminar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpleado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(898, 363);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(890, 334);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(890, 334);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dgEmpleado
+            // 
+            this.dgEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEmpleado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdEmpleado,
+            this.nombreEmpleado,
+            this.FechaNacEmpleado,
+            this.edadEmpleado,
+            this.usuarioEmpleado,
+            this.bEditar,
+            this.bEliminar});
+            this.dgEmpleado.Location = new System.Drawing.Point(12, 370);
+            this.dgEmpleado.Name = "dgEmpleado";
+            this.dgEmpleado.RowHeadersWidth = 51;
+            this.dgEmpleado.RowTemplate.Height = 24;
+            this.dgEmpleado.Size = new System.Drawing.Size(872, 150);
+            this.dgEmpleado.TabIndex = 2;
+            // 
+            // IdEmpleado
+            // 
+            this.IdEmpleado.HeaderText = "Id";
+            this.IdEmpleado.MinimumWidth = 6;
+            this.IdEmpleado.Name = "IdEmpleado";
+            this.IdEmpleado.Width = 125;
+            // 
+            // nombreEmpleado
+            // 
+            this.nombreEmpleado.HeaderText = "Nombre";
+            this.nombreEmpleado.MinimumWidth = 6;
+            this.nombreEmpleado.Name = "nombreEmpleado";
+            this.nombreEmpleado.Width = 125;
+            // 
+            // FechaNacEmpleado
+            // 
+            this.FechaNacEmpleado.HeaderText = "Fecha de Nacimiento";
+            this.FechaNacEmpleado.MinimumWidth = 6;
+            this.FechaNacEmpleado.Name = "FechaNacEmpleado";
+            this.FechaNacEmpleado.Width = 125;
+            // 
+            // edadEmpleado
+            // 
+            this.edadEmpleado.HeaderText = "Edad";
+            this.edadEmpleado.MinimumWidth = 6;
+            this.edadEmpleado.Name = "edadEmpleado";
+            this.edadEmpleado.Width = 125;
+            // 
+            // usuarioEmpleado
+            // 
+            this.usuarioEmpleado.HeaderText = "Usuario";
+            this.usuarioEmpleado.MinimumWidth = 6;
+            this.usuarioEmpleado.Name = "usuarioEmpleado";
+            this.usuarioEmpleado.Width = 125;
+            // 
+            // bEditar
+            // 
+            this.bEditar.HeaderText = "Editar";
+            this.bEditar.MinimumWidth = 6;
+            this.bEditar.Name = "bEditar";
+            this.bEditar.Width = 125;
+            // 
+            // bEliminar
+            // 
+            this.bEliminar.HeaderText = "Eliminar";
+            this.bEliminar.MinimumWidth = 6;
+            this.bEliminar.Name = "bEliminar";
+            this.bEliminar.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(243, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Domicilio";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(369, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(243, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha Nacimiento";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(369, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(243, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(366, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Usuario";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(623, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 38);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 596);
+            this.Controls.Add(this.dgEmpleado);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Abarrotes";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpleado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dgEmpleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdEmpleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreEmpleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaNacEmpleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn edadEmpleado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usuarioEmpleado;
+        private System.Windows.Forms.DataGridViewButtonColumn bEditar;
+        private System.Windows.Forms.DataGridViewButtonColumn bEliminar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
