6e635fa2c0fd7bbf52cb81dedcdcd6ed1eff3260
Luis Carlos Bacasehua <luca.bacasehua@gmail.com>
2019-09-17 17:37:11 -0500

    Update tiendaDeAbarrotes.sql
    Creación de la base de datos, dos esquemas: Administracion y Transaccion y creación de las tablas Empleado y Ventas.

--- tiendaDeAbarrotes.sql
+++ tiendaDeAbarrotes.sql
@@ -1,1 +1,26 @@
 --Scrip para la base de datos de una tienda de abarrotes--
+CREATE DATABASE TiendaAbarrotes
+USE TiendaAbarrotes
+
+CREATE SCHEMA Administracion
+CREATE SCHEMA Transaccion
+
+CREATE TABLE Administracion.Empleado(
+	IdEmpleado BIGINT IDENTITY(1,1) NOT NULL,
+	Nombre VARCHAR(200) NOT NULL,
+	Domicilio VARCHAR(200) NOT NULL,
+	FechaNac DATE NOT NULL,
+	Edad INT,
+	Usuario VARCHAR(200) NOT NULL,
+	Contrasenia VARCHAR(100) NOT NULL,
+	CONSTRAINT PK_EMPLEADO PRIMARY KEY(IdEmpleado)
+)
+
+CREATE TABLE Transaccion.Venta(
+	IdVenta BIGINT IDENTITY(1,1) NOT NULL,
+	IdEmpleado BIGINT NOT NULL,
+	FechaVenta DATE NOT NULL,
+	Total REAL NOT NULL,
+	CONSTRAINT PK_VENTA PRIMARY KEY(IdVenta),
+	CONSTRAINT FK_EMPLEADO FOREIGN KEY(IdEmpleado) REFERENCES Administracion.Empleado(IdEmpleado)
+)
