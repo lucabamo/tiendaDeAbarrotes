519546eb4ecde3a3b8b88ced8d5db187042550e9
Karla Rosas <k2.rosas.v@gmail.com>
2019-09-22 17:57:01 -0500

    Tabla Venta

--- sistemaTiendaAbarrotes/Form1.cs
+++ sistemaTiendaAbarrotes/Form1.cs
@@ -14,9 +14,11 @@ namespace sistemaTiendaAbarrotes
     public partial class Form1 : Form
     {
         SqlConnection conexion;
+        Venta venta;
         public Form1()
         {
             InitializeComponent();
+            venta = new Venta();
             conectar();
 
         }
@@ -25,7 +27,7 @@ namespace sistemaTiendaAbarrotes
         {
             string connectionString = null;
             //Cadena de conexión
-            connectionString = "Server=LAPTOP-M8A5375A\\SQLEXPRESS; Database = TiendaAbarrotes; Trusted_Connection = true;";
+            connectionString = "Server=DESKTOP-AP88PFE\\SQLEXPRESS; Database = TiendaAbarrotes; Trusted_Connection = true;";
 
             conexion = new SqlConnection(connectionString);
             try
@@ -39,5 +41,52 @@ namespace sistemaTiendaAbarrotes
             }
         }
 
+        private void btAgregarVenta_Click(object sender, EventArgs e)
+        {
+            dgVentas.DataSource = "";
+            venta.insertVenta(conexion, tbIdEmpleado.Text, tbFechaVenta.Text, tbTotal.Text);
+            dgVentas.DataSource = venta.selectVentas(conexion);
+            resetTabVentas();
+        }
+
+        private void btModificarVenta_Click(object sender, EventArgs e)
+        {
+            dgVentas.DataSource = "";
+            venta.updateVenta(conexion, tbIdEmpleado.Text, tbFechaVenta.Text, tbTotal.Text, venta.IdVenta);
+            dgVentas.DataSource =  venta.selectVentas(conexion);
+            resetTabVentas();
+        }
+
+        private void btEliminarVenta_Click(object sender, EventArgs e)
+        {
+            dgVentas.DataSource = "";
+            venta.deleteVenta(conexion, venta.IdVenta);
+            dgVentas.DataSource = venta.selectVentas(conexion);
+            resetTabVentas();
+        }
+
+        private void dgVentas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < dgVentas.Rows.Count - 1 && dgVentas.Rows[e.RowIndex].Cells[0].Value != null)
+            {
+                venta.IdVenta = dgVentas.Rows[e.RowIndex].Cells[0].Value.ToString();
+                tbIdEmpleado.Text = dgVentas.Rows[e.RowIndex].Cells[1].Value.ToString();
+                tbFechaVenta.Text = dgVentas.Rows[e.RowIndex].Cells[2].Value.ToString();
+                tbTotal.Text = dgVentas.Rows[e.RowIndex].Cells[3].Value.ToString();
+            }
+        }
+
+        private void tabVenta_Enter(object sender, EventArgs e)
+        {
+            dgVentas.DataSource = "";
+            dgVentas.DataSource = venta.selectVentas(conexion);
+        }
+
+        private void resetTabVentas()
+        {
+            tbIdEmpleado.Text = "";
+            tbFechaVenta.Text = "";
+            tbTotal.Text = "";
+        }
     }
 }
---
+++ sistemaTiendaAbarrotes/Venta.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistemaTiendaAbarrotes
+{
+    public class Venta
+    {
+        private string idVenta;
+
+        public Venta()
+        {
+            idVenta = "";
+        }
+
+        public DataTable selectVentas(SqlConnection connection)
+        {
+            string query = "SELECT * FROM Transaccion.Venta";
+            DataTable table = new DataTable();
+
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            adapter.Fill(table);
+
+            return table; 
+        }
+
+        public void insertVenta(SqlConnection connection,string idEmpleado, string fechaVenta, string total)
+        {
+            string query = "INSERT INTO Transaccion.Venta (IdEmpleado, FechaVenta, Total)" +
+                "VALUES (@idEmpleado, @fechaVenta, @total)";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@idEmpleado", idEmpleado);
+            command.Parameters.AddWithValue("@fechaVenta", fechaVenta);
+            command.Parameters.AddWithValue("@total", total);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Agregado Correctamente");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void updateVenta(SqlConnection connection, string idEmpleado, string fechaVenta, string total, string idVenta)
+        {
+            string query = "UPDATE Transaccion.Venta SET IdEmpleado=@idEmpleado, FechaVenta = @fechaVenta, Total = @total WHERE IdVenta = @idVenta";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@idEmpleado", idEmpleado);
+            command.Parameters.AddWithValue("@fechaVenta", fechaVenta);
+            command.Parameters.AddWithValue("@total", total);
+            command.Parameters.AddWithValue("@idVenta", idVenta);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Modificado correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void deleteVenta(SqlConnection connection, string idVenta)
+        {
+            string query = "DELETE FROM Transaccion.Venta WHERE IdVenta = @idVenta";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@idVenta", idVenta);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Se ha eliminado correctamente");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public string IdVenta { get => idVenta; set => idVenta = value; }
+    }
+}
